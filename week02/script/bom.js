b40247a560567cdf36ac46d22cdbfc22feb35e17
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-03-13 00:04:50 +0100

    bom.js added

--- week02/script/bom.js
+++ week02/script/bom.js
@@ -1,18 +1,23 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#button');
-const list = document.querySelector('#chapterList');
-button.addEventListener("click", () => {
-    if (input.value.trim() === "") {
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+button.addEventListener("click", function(){
+    if (input.value.trim() !== "") {
         alert("Please enter a chapter name.");
+        input.focus();
         return;
     }
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+const li = document.createElement("li");
+li.textContent = input.value;
+
+const deleteButton = document.createElement("button");
 deleteButton.textContent = "❌";
-    deleteButton.classList.add("delete");
-    deleteButton.addEventListener("click", () => {
+deleteButton.classList.add("delete");
+
+deleteButton.addEventListener("click", function(){
         list.removeChild(li);
-    });
+        input.focus();
+});
 
     li.append(deleteButton);
     list.append(li);
